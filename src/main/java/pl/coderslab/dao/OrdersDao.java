2b93917b667914543283bce1c6906d86b474105c
Kamil Kozlowski <keymillo@gmail.com>
2018-08-14 18:25:00 +0200

    Poprawki

--- src/main/java/pl/coderslab/dao/OrdersDao.java
+++ src/main/java/pl/coderslab/dao/OrdersDao.java
@@ -1,60 +1,60 @@
-//package pl.coderslab.dao;
-//
-//import pl.coderslab.model.Orders;
-//import pl.coderslab.service.DbService;
-//
-//import java.sql.SQLException;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class OrdersDao {
-//
-//    public static void save(Orders order){
-//        if(order.getId()==0){
-//            String query = "INSERT INTO Orders(employee_id, vehicle_id, status_id, service_accept, service_plan, issue_description) VALUES (?,?,?,NOW(),?,?)";
-//            List<String> params = new ArrayList<>();
-//            params.add(String.valueOf(order.getEmployee().getId()));
-//            params.add(String.valueOf(order.getVehicle().getId()));
-//            params.add(String.valueOf(order.getStatus().getId()));
-//            params.add(order.getServicePlan());
-//            params.add(order.getIssueDesc());
-//
-//            try {
-//                Integer id = DbService.insertIntoDatabase(query,params);
-//                if(id!=null){
-//                    order.setId(id);
-//                }
-//            } catch (SQLException e) {
-//                e.printStackTrace();
-//            }
-//
-//        }else{
-//            String query = "UPDATE Orders SET employee_id = ?, vehicle_id = ?, status_id = ?, service_accept = ?," +
-//                    "service_plan = ?, service_start = ?, issue_description = ?, repair_description = ?, " +
-//                    "repair_cost = ?, parts_cost = ?, hourly_rate = ?, man_hours = ? WHERE order_id = ?";
-//            List<String> params = new ArrayList<>();
-//            params.add(String.valueOf(order.getEmployee().getId()));
-//            params.add(String.valueOf(order.getVehicle().getId()));
-//            params.add(String.valueOf(order.getStatus().getId()));
-//            params.add(order.getServiceAccept());
-//            params.add(order.getServicePlan());
-//            params.add(order.getServiceStart());
-//            params.add(order.getIssueDesc());
-//            params.add(order.getRepairDesc());
-//            params.add(String.valueOf(order.getRepairCost()));
-//            params.add(String.valueOf(order.getPartsCost()));
-//            params.add(String.valueOf(order.getHourlyRate()));
-//            params.add(String.valueOf(order.getManHours()));
-//            params.add(String.valueOf(order.getId()));
-//            try{
-//                DbService.executeUpdate(query,params);
-//
-//            }catch (SQLException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//
-//    }
-//
-//
-//}
+package pl.coderslab.dao;
+
+import pl.coderslab.model.Orders;
+import pl.coderslab.service.DbService;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrdersDao {
+
+    public static void save(Orders order){
+        if(order.getId()==0){
+            String query = "INSERT INTO Orders(employee_id, vehicle_id, status_id, service_accept, service_plan, issue_description) VALUES (?,?,?,NOW(),?,?)";
+            List<String> params = new ArrayList<>();
+            params.add(String.valueOf(order.getEmployee().getId()));
+            params.add(String.valueOf(order.getVehicle().getId()));
+            params.add(String.valueOf(order.getStatus().getId()));
+            params.add(order.getServicePlan());
+            params.add(order.getIssueDesc());
+
+            try {
+                Integer id = DbService.insertIntoDatabase(query,params);
+                if(id!=null){
+                    order.setId(id);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        }else{
+            String query = "UPDATE Orders SET employee_id = ?, vehicle_id = ?, status_id = ?, service_accept = ?," +
+                    "service_plan = ?, service_start = ?, issue_description = ?, repair_description = ?, " +
+                    "repair_cost = ?, parts_cost = ?, hourly_rate = ?, man_hours = ? WHERE order_id = ?";
+            List<String> params = new ArrayList<>();
+            params.add(String.valueOf(order.getEmployee().getId()));
+            params.add(String.valueOf(order.getVehicle().getId()));
+            params.add(String.valueOf(order.getStatus().getId()));
+            params.add(order.getServiceAccept());
+            params.add(order.getServicePlan());
+            params.add(order.getServiceStart());
+            params.add(order.getIssueDesc());
+            params.add(order.getRepairDesc());
+            params.add(String.valueOf(order.getRepairCost()));
+            params.add(String.valueOf(order.getPartsCost()));
+            params.add(String.valueOf(order.getHourlyRate()));
+            params.add(String.valueOf(order.getManHours()));
+            params.add(String.valueOf(order.getId()));
+            try{
+                DbService.executeUpdate(query,params);
+
+            }catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+}
--- src/main/java/pl/coderslab/model/Orders.java
+++ src/main/java/pl/coderslab/model/Orders.java
@@ -1,145 +1,145 @@
-//package pl.coderslab.model;
-//
-//public class Orders {
-//
-//    private int id;
-//    private Employee employee;
-//    private Vehicle vehicle;
-//    private Status status;
-//    private String serviceAccept;
-//    private String servicePlan;
-//    private String serviceStart;
-//    private String issueDesc;
-//    private String repairDesc;
-//    private double repairCost;
-//    private double partsCost;
-//    private double hourlyRate;
-//    private int manHours;
-//
-//    public Orders() {
-//    }
-//
-//    public Orders(Vehicle vehicle, String serviceAccept, String issueDesc, Status status) {
-//        this.id = 0;
-//        setVehicle(vehicle);
-//        setServiceAccept(serviceAccept);
-//        setIssueDesc(issueDesc);
-//        setStatus(status);
-//    }
-//
-//    public Orders(Vehicle vehicle, String serviceAccept, String issueDesc, Status status, Employee employee) {
-//        this(vehicle, serviceAccept, issueDesc, status);
-//        setEmployee(employee);
-//    }
-//
-//    public Orders(Vehicle vehicle, String serviceAccept, String issueDesc, Status status, Employee employee, String servicePlan) {
-//        this(vehicle, serviceAccept, issueDesc, status, employee);
-//        setServicePlan(servicePlan);
-//    }
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    public Employee getEmployee() {
-//        return employee;
-//    }
-//
-//    public void setEmployee(Employee employee) {
-//        this.employee = employee;
-//    }
-//
-//    public Vehicle getVehicle() {
-//        return vehicle;
-//    }
-//
-//    public void setVehicle(Vehicle vehicle) {
-//        this.vehicle = vehicle;
-//    }
-//
-//    public Status getStatus() {
-//        return status;
-//    }
-//
-//    public void setStatus(Status status) {
-//        this.status = status;
-//    }
-//
-//    public String getServiceAccept() {
-//        return serviceAccept;
-//    }
-//
-//    public void setServiceAccept(String serviceAccept) {
-//        this.serviceAccept = serviceAccept;
-//    }
-//
-//    public String getServicePlan() {
-//        return servicePlan;
-//    }
-//
-//    public void setServicePlan(String servicePlan) {
-//        this.servicePlan = servicePlan;
-//    }
-//
-//    public String getServiceStart() {
-//        return serviceStart;
-//    }
-//
-//    public void setServiceStart(String serviceStart) {
-//        this.serviceStart = serviceStart;
-//    }
-//
-//    public String getIssueDesc() {
-//        return issueDesc;
-//    }
-//
-//    public void setIssueDesc(String issueDesc) {
-//        this.issueDesc = issueDesc;
-//    }
-//
-//    public String getRepairDesc() {
-//        return repairDesc;
-//    }
-//
-//    public void setRepairDesc(String repairDesc) {
-//        this.repairDesc = repairDesc;
-//    }
-//
-//
-//    public double getRepairCost() {
-//        return repairCost;
-//    }
-//
-//    public void setRepairCost(double repairCost) {
-//        this.repairCost = repairCost;
-//    }
-//
-//    public double getPartsCost() {
-//        return partsCost;
-//    }
-//
-//    public void setPartsCost(double partsCost) {
-//        this.partsCost = partsCost;
-//    }
-//
-//    public double getHourlyRate() {
-//        return hourlyRate;
-//    }
-//
-//    public void setHourlyRate(double hourlyRate) {
-//        this.hourlyRate = hourlyRate;
-//    }
-//
-//    public int getManHours() {
-//        return manHours;
-//    }
-//
-//    public void setManHours(int manHours) {
-//        this.manHours = manHours;
-//    }
-//
-//}
+package pl.coderslab.model;
+
+public class Orders {
+
+    private int id;
+    private Employee employee;
+    private Vehicle vehicle;
+    private Status status;
+    private String serviceAccept;
+    private String servicePlan;
+    private String serviceStart;
+    private String issueDesc;
+    private String repairDesc;
+    private double repairCost;
+    private double partsCost;
+    private double hourlyRate;
+    private int manHours;
+
+    public Orders() {
+    }
+
+    public Orders(Vehicle vehicle, String serviceAccept, String issueDesc, Status status) {
+        this.id = 0;
+        setVehicle(vehicle);
+        setServiceAccept(serviceAccept);
+        setIssueDesc(issueDesc);
+        setStatus(status);
+    }
+
+    public Orders(Vehicle vehicle, String serviceAccept, String issueDesc, Status status, Employee employee) {
+        this(vehicle, serviceAccept, issueDesc, status);
+        setEmployee(employee);
+    }
+
+    public Orders(Vehicle vehicle, String serviceAccept, String issueDesc, Status status, Employee employee, String servicePlan) {
+        this(vehicle, serviceAccept, issueDesc, status, employee);
+        setServicePlan(servicePlan);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getServiceAccept() {
+        return serviceAccept;
+    }
+
+    public void setServiceAccept(String serviceAccept) {
+        this.serviceAccept = serviceAccept;
+    }
+
+    public String getServicePlan() {
+        return servicePlan;
+    }
+
+    public void setServicePlan(String servicePlan) {
+        this.servicePlan = servicePlan;
+    }
+
+    public String getServiceStart() {
+        return serviceStart;
+    }
+
+    public void setServiceStart(String serviceStart) {
+        this.serviceStart = serviceStart;
+    }
+
+    public String getIssueDesc() {
+        return issueDesc;
+    }
+
+    public void setIssueDesc(String issueDesc) {
+        this.issueDesc = issueDesc;
+    }
+
+    public String getRepairDesc() {
+        return repairDesc;
+    }
+
+    public void setRepairDesc(String repairDesc) {
+        this.repairDesc = repairDesc;
+    }
+
+
+    public double getRepairCost() {
+        return repairCost;
+    }
+
+    public void setRepairCost(double repairCost) {
+        this.repairCost = repairCost;
+    }
+
+    public double getPartsCost() {
+        return partsCost;
+    }
+
+    public void setPartsCost(double partsCost) {
+        this.partsCost = partsCost;
+    }
+
+    public double getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(double hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    public int getManHours() {
+        return manHours;
+    }
+
+    public void setManHours(int manHours) {
+        this.manHours = manHours;
+    }
+
+}
