abb28fd8ad7e14288b503c26298478873c2867c5
Grzegorz Krawczynski <grzegorzkrawczynski@yahoo.com>
2018-08-14 11:51:36 +0200

    Added classes: Employee, EmployeDao

---
+++ src/main/java/pl/coderslab/model/Employee.java
@@ -0,0 +1,81 @@
+package pl.coderslab.model;
+
+public class Employee {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String phone;
+    private String note;
+    private double hourly_rate;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String firstName, String lastName, String address, String phone, String note, double hourly_rate) {
+        this.id = 0;
+        setFirstName(firstName);
+        setLastName(lastName);
+        setAddress(address);
+        setPhone(phone);
+        setNote(note);
+        setHourly_rate(hourly_rate);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public double getHourly_rate() {
+        return hourly_rate;
+    }
+
+    public void setHourly_rate(double hourly_rate) {
+        this.hourly_rate = hourly_rate;
+    }
+}
